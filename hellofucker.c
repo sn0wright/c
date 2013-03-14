09e15a2e9a539717612961d631f85464298edb32
Snow Wright <silastitus@gmail.com>
2013-03-13 20:11:28 -0400

    i dont get it

---
+++ hellofucker.c
@@ -0,0 +1,12 @@
+/*this is my attempt at learning how to code in c */
+
+/* Hello World program */
+
+#include<stdio.h>
+
+main()
+{
+    printf("fucker");
+
+
+}
---
+++ shutdown.c
@@ -0,0 +1,7 @@
+ #include<stdio.h>
+#include<dos.h>
+
+int main (void){
+    system("shutdown /r");
+    return 0;
+}
